0c8657e85869514aedeb2b74cf1f326e7974485c
ivan <ivan2adjiri@gmail.com>
2023-09-07 16:12:09 +0000

    React redux to-do-list

---
+++ build/precache-manifest.7a1a899b84f1fda4c7dcd13a03e92369.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "08c37d7f02b4b58397a9",
+    "url": "/static/css/main.e4b8bf57.chunk.css"
+  },
+  {
+    "revision": "08c37d7f02b4b58397a9",
+    "url": "/static/js/main.f743f0c1.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "b85f8a6f8417e149d2f8",
+    "url": "/static/js/2.eb8ab42f.chunk.js"
+  },
+  {
+    "revision": "9af7b2fa991057afb5f07fc3604f0f92",
+    "url": "/index.html"
+  }
+];
